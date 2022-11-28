3d8db3ad64110239f92b2ba7674c5e3b7b264ed0
Waqar Muhammad <muhammad.s.waqar@gmail.com>
2022-11-28 17:30:07 -0500

    Added questions 1 and question 2.

---
+++ Q1.py
@@ -0,0 +1,21 @@
+# Q1
+# Leetcode 74. search a 2D Matrix
+
+# Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than the last integer of the previous row.
+
+# example 1:
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+# Output: [0,1]
+
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+# Output: [-1,-1]
+
+# Constraints:
+
+# m == matrix.length
+# n == matrix[i].length
+# 1 <= m, n <= 100
+# -104 <= matrix[i][j], target <= 104
